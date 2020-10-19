30efe2d7309f75081c5f698cf5657f9e30c055cd
David Heizer <DavidHeizer@QuickenLoans.com>
2020-10-19 16:29:37 -0400

    IDAL done, DB tables modeled

---
+++ BootcampFlashcards/Models/Favorite.cs
@@ -0,0 +1,17 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampFlashcards.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public long id { get; set; }
+        public string username { get; set; }
+        public long questionID { get; set; }
+
+    }
+}
